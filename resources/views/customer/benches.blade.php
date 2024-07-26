eeace3c4eb73514160bac049899e2aeb00f7c297
yuraludchak <yura.ludchak@ralabs.org>
2024-07-26 16:33:37 +0300

    time for call

--- resources/views/customer/benches.blade.php
+++ resources/views/customer/benches.blade.php
@@ -8,10 +8,10 @@
                     @foreach($users as $user)
                         <li class="flex justify-between gap-x-6 py-5 mb-5 bg-gray-800 p-5">
                             <div class="flex min-w-0 gap-x-4">
-                                <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
-                                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                     alt="">
-                                <div class="min-w-0 flex-auto">
+                                <div class="flex flex-col gap-2">
+                                    <img class="h-12 w-12 flex-none rounded-full bg-gray-50"
+                                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                         alt="">
                                     <p class="text-sm font-semibold leading-6 text-gray-300">{{$user->name}}</p>
                                 </div>
                                 <div class="flex flex-wrap gap-1">
